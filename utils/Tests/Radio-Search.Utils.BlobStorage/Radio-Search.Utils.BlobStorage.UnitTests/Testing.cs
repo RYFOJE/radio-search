9cbf557c807c5d4ed0f8b98e1b59b59ec03ca916
Ryan Jennings <ryfoje2@gmail.com>
2025-09-09 16:40:55 -0400

    Attempting to have tests auto run

---
+++ utils/Tests/Radio-Search.Utils.BlobStorage/Radio-Search.Utils.BlobStorage.UnitTests/Testing.cs
@@ -0,0 +1,11 @@
+namespace Radio_Search.Utils.BlobStorage.UnitTests
+{
+    public class Testing
+    {
+        [Fact]
+        public void TestingUnitTests()
+        {
+            Assert.True(true);
+        }
+    }
+}
