b5228495c0838806e2ead0eb1e37d0c9d194df6a
Ryan Jennings <ryfoje2@gmail.com>
2025-09-30 18:03:56 -0400

    Removed importing of not required file

--- services/Radio-Search.Importer/Radio-Search.Importer.Canada/Radio-Search.Importer.Canada.Function/Program.cs
+++ services/Radio-Search.Importer/Radio-Search.Importer.Canada/Radio-Search.Importer.Canada.Function/Program.cs
@@ -42,7 +42,6 @@ var isProduction = string.Equals(environment, "production");
 var config = new ConfigurationBuilder()
     .SetBasePath(AppContext.BaseDirectory)
     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-    .AddJsonFile($"{environment}.settings.json", optional: true, reloadOnChange: true)
     .AddEnvironmentVariables()
     .Build();
 #endregion
