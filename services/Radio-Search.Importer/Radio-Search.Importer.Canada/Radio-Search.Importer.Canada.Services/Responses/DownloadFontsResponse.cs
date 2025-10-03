b96f28a3136db5dc9ae435daa32f322473528b3f
Ryan Jennings <ryfoje2@gmail.com>
2025-10-02 20:38:39 -0400

    Basic request contract

---
+++ services/Radio-Search.Importer/Radio-Search.Importer.Canada/Radio-Search.Importer.Canada.Services/Responses/DownloadFontsResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Radio_Search.Importer.Canada.Services.Responses
+{
+    public class DownloadFontsResponse
+    {
+        public List<string> DownloadedFontPaths { get; set; } = [];
+        public List<string> DownloadedFonts { get; set; } = [];
+        public string DownloadedFontsRootDirectory { get; set; } = "";
+        public int TotalFontsDownloaded { get; set; } = 0;
+    }
+}
