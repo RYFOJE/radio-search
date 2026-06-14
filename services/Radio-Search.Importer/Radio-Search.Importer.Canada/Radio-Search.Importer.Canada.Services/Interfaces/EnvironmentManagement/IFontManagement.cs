67b5541dc71864fedb77da12f709cf76773e2dc0
Ryan Jennings <ryfoje2@gmail.com>
2026-06-14 12:16:21 -0400

    Fixed fonts not being available on start

---
+++ services/Radio-Search.Importer/Radio-Search.Importer.Canada/Radio-Search.Importer.Canada.Services/Interfaces/EnvironmentManagement/IFontManagement.cs
@@ -0,0 +1,14 @@
+﻿namespace Radio_Search.Importer.Canada.Services.Interfaces.EnvironmentManagement
+{
+    /// <summary>
+    /// Font management service that allows the application to control the fonts available to it
+    /// </summary>
+    public interface IFontManagement
+    {
+        /// <summary>
+        /// Ensures all required fonts are installed and available to the application
+        /// </summary>
+        /// <returns></returns>
+        void InitializaFonts();
+    }
+}
